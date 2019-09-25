e413dc6e52e8727a455944eb5658758bbe55d627
Dylan Bartlett <2047912+bartlettD@users.noreply.github.com>
2019-09-25 23:00:54 +0100

    Trying to implement the plugboard swapping of graphemes
    Current implementation doesn't work. Need to figure out an
    efficient way of replacing individual graphemes with their
    partner in the plugboard.
    
    It might be that I have to fall back to using ASCII strings
    rather than Rust default UTF-8 to make things easier. This
    IS a simulator for Enigma so its Latin chars only.
    
    Although supporting Japanese editions using this method might
    be harder...

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,25 @@
-use std::collections::HashMap;
-
 #[derive(Debug)]
-struct Plug(char, char);
+struct Plug(String, String); //Struct to hold plugboard settings.
 
 fn main() {
-    println!("Hello, world!");
-	let pluga = Plug('A', 'B');
-	let plugb = Plug('c','d');
+
+	let pluga = Plug("A".to_string(), 'B'.to_string());
+	let plugb = Plug('c'.to_string(),'d'.to_string());
 	println!("Pluga : {:?}", pluga);
 	
-	let Plugboard: Vec<Plug> = vec![pluga,plugb];
-	println!("Plugboard Settings: {:?}", Plugboard);
+	let plugboard: Vec<Plug> = vec![pluga,plugb];
+	println!("Plugboard Settings: {:?}", plugboard);
+
+	let mut string = String::from("ABBA");
+
+	println!("{}",string);
+	plugboard_transform(&mut string, plugboard);
+	println!("{}",string);
+}
+
+fn plugboard_transform(s: &mut String, settings: Vec<Plug>) {
+	let settings_iter = settings.iter();
+	for set in settings_iter {
+		*s = s.replace(&set.0 , &set.1);
+	}
 }
